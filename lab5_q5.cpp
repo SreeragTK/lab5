f8ba64460479386175b10f6e9a8f4f265dea803f
SreeragTK <42139412+SreeragTK@users.noreply.github.com>
2018-09-01 11:40:07 +0530

    Update lab5_q5.cpp

--- lab5_q5.cpp
+++ lab5_q5.cpp
@@ -1,8 +1,13 @@
+//include library
 #include<iostream>
 using namespace std;
+//include main function
 int main(){
+//define variables
 int a;
+//assign values to the variables
 cout<<"Enter your number:";cin>>a;
+//apply conditions
 if(a%2==0)
 cout<<"your number is even";
 else
