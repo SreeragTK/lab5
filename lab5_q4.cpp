f8090aeaf8319f395fb2b1878f3ca1dbfb2caab9
SreeragTK <42139412+SreeragTK@users.noreply.github.com>
2018-09-01 11:35:41 +0530

    Update lab5_q4.cpp

--- lab5_q4.cpp
+++ lab5_q4.cpp
@@ -1,9 +1,14 @@
+//include library
 #include<iostream>
 using namespace std;
+//include main function
 int main(){
+//define variables
 int a;
+  //asign values to variable
 cout<<"enter your number:";
 cin>>a;
+//apply conditions
 if(a%55==0)
 cout<<"your number is divisible by 5 and 11";
 else
