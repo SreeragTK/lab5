9555f9c172c6a34fa3954ad5ef62604b977d08e1
SreeragTK <42139412+SreeragTK@users.noreply.github.com>
2018-09-01 11:47:51 +0530

    Update lab5_q3.cpp

--- lab5_q3.cpp
+++ lab5_q3.cpp
@@ -1,8 +1,13 @@
+//include library
 #include <iostream>
 using namespace std;
+//include main function
 int main(){
+//include variables
 float a;
+//give values to the variables
 cout<<"Enter your number:";cin>>a;
+//apply conditions
 if(a>0)
 cout<<"your number is positive";
 else if(a==0)
