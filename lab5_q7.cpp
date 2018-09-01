5373bdbe33854394a1793010f7c5456a6d10ba76
SreeragTK <42139412+SreeragTK@users.noreply.github.com>
2018-09-01 11:43:18 +0530

    Update lab5_q7.cpp

--- lab5_q7.cpp
+++ lab5_q7.cpp
@@ -1,9 +1,14 @@
+//include library
 #include <iostream>
 using namespace std;
+//include main function
 int main(){
+//include variables
 char a;
+//assign value to the variable
 cout<<"Enter something:";
 cin>>a;
+//apply conditions
 if (a>=('a'or'A') or a<=('z'or 'Z'))
 {cout<<"The entered things only related to alphabets";}
 else
