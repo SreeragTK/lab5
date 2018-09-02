93e81e72d7d7726ac1eddd4f980ff6dee540cc5c
SreeragTK <42139412+SreeragTK@users.noreply.github.com>
2018-09-02 13:26:34 +0530

    Add files via upload

---
+++ lab5_q25.cpp
@@ -0,0 +1,17 @@
+//include library
+#include <iostream>
+using namespace std;
+//include main function
+int main(){
+//include variables
+float a;
+//assign value to the variable
+a=2;
+//apply conditions
+while (a<=100)
+{cout<<"The even numbers between 1 & 100 are:"<<a<<"\n";
+a=a+2;}
+return 0;
+}
+
+
