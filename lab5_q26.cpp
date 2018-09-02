ba6a5df0860880d6652209360be96b88b60071ce
SreeragTK <42139412+SreeragTK@users.noreply.github.com>
2018-09-02 13:34:40 +0530

    Add files via upload

---
+++ lab5_q26.cpp
@@ -0,0 +1,17 @@
+//include library
+#include <iostream>
+using namespace std;
+//include main function
+int main(){
+//include variables
+float a;
+//assign value to the variable
+a=1;
+//apply conditions
+while (a<=100)
+{cout<<"The odd numbers between 1 & 100 are:"<<a<<"\n";
+a=a+2;}
+return 0;
+}
+
+
