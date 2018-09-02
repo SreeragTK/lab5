a071970e766c2c39f22a7d7acb885b7f6a69b779
SreeragTK <42139412+SreeragTK@users.noreply.github.com>
2018-09-02 12:49:06 +0530

    Add files via upload

---
+++ lab5_q24..cpp
@@ -0,0 +1,17 @@
+//include library
+#include <iostream>
+using namespace std;
+//include main function
+int main(){
+//include variables
+char ab,b='a';
+//assign value to the variable
+ab=b;
+//apply conditions
+while (ab<='z')
+{cout<<"The english alphabets are:"<<ab<<"\n";
+ab++;}
+return 0;
+}
+
+
